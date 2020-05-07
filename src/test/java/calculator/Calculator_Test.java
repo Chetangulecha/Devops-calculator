66d61684867ccd24da16b74e9c0612ecc95e2af0
sahilarora1995 <sarora160@gmail.com>
2020-05-07 16:34:59 +0530

    Create Calculator_Test.java

---
+++ src/test/java/calculator/Calculator_Test.java
@@ -0,0 +1,23 @@
+package calculator;
+
+import org.junit.Before;
+
+import org.junit.*;
+
+public class Calculator_Test {
+
+    private Calc calc;
+    @Before
+    public void setUp() {
+        calc = new Calc();
+    }
+
+    @Test
+    public void testAdd() {
+        int a = 12;
+        int b = 21;
+        int expectedResult = 33;
+        long result = calc.add(a, b);
+        Assert.assertEquals(expectedResult, result);;
+    }
+}
