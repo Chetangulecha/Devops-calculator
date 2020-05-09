26f3f00aab9b27d62f9c7a305dfa70db05bb239f
sahilarora1995 <sarora160@gmail.com>
2020-05-09 11:00:50 +0530

    Update AppTest.java

--- src/test/java/calculator/AppTest.java
+++ src/test/java/calculator/AppTest.java
@@ -1,20 +1,22 @@
 package calculator;
-
 import static org.junit.Assert.assertTrue;
+import org.junit.Before;
 
-import org.junit.Test;
+public class AppTest {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+    private Main calc;
+    @Before
+    public void setUp() {
+        calc = new Main();
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testAdd() {
+        int a = 12;
+        int b = 21;
+        int expectedResult = 33;
+        long result = calc.add(a, b);
+        Assert.assertEquals(expectedResult, result);;
     }
 }
+
